e09a02034ae95b1d0b44449e77e02a5bf5d2c545
tetchi <tetchi@hey.com>
2025-07-25 11:16:13 -0400

    Disable localeDetection
    Fixes locale change on refresh.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
-    i18n: {
-      defaultLocale: 'fr',
-      locales: ['en', 'fr'],
-    },
-    localePath: path.resolve('./public/locales')
-  }
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr', 'en'],
+    localeDetection: false,
+  },
+  localePath: path.resolve('./public/locales')
+}
+
