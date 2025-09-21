60bb7525355affb5172b7fbb7f5015fa58559c4b
tetchi <tetchi@hey.com>
2025-09-21 13:17:48 -0400

    Add metadata

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link> 
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <meta desc={t('meta_desc')} />
       </Head>
       <div className="absolute top-4 right-4">
         <Link href="/" locale={changeTo} className="
